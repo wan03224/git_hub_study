c927d18077cd1ebf9978d459ca6e74e0bbc8244e
wan03224 <dongwan03224@gmail.com>
2025-10-15 11:36:57 +0900

    test.java 파일 수정 3

--- src/test.java
+++ src/test.java
@@ -1,9 +1,6 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("test 출력");
-		System.out.println("test 출력");
-		System.out.println("test 출력");
-		System.out.println("test 출력");
 	}
 }
 
